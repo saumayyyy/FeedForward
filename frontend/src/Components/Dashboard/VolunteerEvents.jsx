9781d719457fd142844138578afde48d33fc3279
Saumayyyy <saumayaggarwal6@gmail.com>
2024-09-05 08:04:10 +0530

    events fixed

--- frontend/src/Components/Dashboard/VolunteerEvents.jsx
+++ frontend/src/Components/Dashboard/VolunteerEvents.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getVolunteerEvents } from '../../operations/eventApi'; // Adjust the import path as needed
-import { formatDate } from '../../operations/formatDate';
 import { useNavigate } from 'react-router-dom';
+import EventsShow from '../EventsShow';
 
 const VolunteerEvents = () => {
   const [events, setEvents] = useState([]);
@@ -23,73 +23,7 @@ const VolunteerEvents = () => {
   }, [token]);
 
   return (
-    <div className="p-8">
-      <div className="text-4xl font-bold text-white mb-6">Enrolled Events</div>
-      {!events ? (
-        <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
-          <div className="spinner"></div>
-        </div>
-      ) : !events.length ? (
-        <p className="grid h-[10vh] w-full place-content-center text-white-500">
-          You have not enrolled in any event yet.
-          {/* TODO: Modify this Empty State */}
-        </p>
-      ) : (
-        <div className="overflow-x-auto">
-          {/* Headings */}
-          <div className="grid grid-cols-5 bg-gray-800 text-white rounded-t-lg p-4 border-b-2 border-white">
-            <p className="col-span-2 font-semibold">Event Details</p>
-            <p className="col-span-1 text-center font-semibold">Event Date</p>
-            <p className="col-span-1 text-center font-semibold">Status</p>
-            <p className="col-span-1 text-center font-semibold">Creation Date</p>
-          </div>
-          {/* Event Rows */}
-          {events.map((event, i) => (
-            <div
-              className={`grid grid-cols-5 items-center border-b border-white p-4 hover:bg-gray-100 transition`}
-              key={i}
-            >
-              <div
-                className="col-span-2 flex cursor-pointer items-center gap-4"
-                onClick={() => {
-                  navigate(`/event/${event._id}`);
-                }}
-              >
-                <img
-                  src={event.image}
-                  alt="event_img"
-                  className="h-16 w-16 rounded-lg object-cover"
-                />
-                <div className="flex flex-col gap-1">
-                  <p className="font-bold text-lg text-white">{event.title}</p>
-                  <p className="text-sm text-white">Type of Food: {event.typeOfFood}</p>
-                  <p className="text-sm text-white">Location: {event.location}</p>
-                  <p className="text-sm text-white">Quantity: {event.quantity}</p>
-                  <p className="text-sm text-white">Contact: {event.contactDetails}</p>
-                </div>
-              </div>
-              <div className="col-span-1 text-center text-white text-sm">
-                {formatDate(event.eventDate)}
-              </div>
-              <div className="col-span-1 text-center text-sm">
-                <span
-                  className={`px-3 py-4 text-[15px] rounded-full ${
-                    event.status === 'completed'
-                      ? 'bg-caribbeangreen-200 text-caribbeangreen-800'
-                      : 'bg-yellow-200 text-yellow-800'
-                  }`}
-                >
-                  {event.status === 'completed' ? 'Completed' : 'Available'}
-                </span>
-              </div>
-              <div className="col-span-1 text-center text-white text-sm">
-                {formatDate(event.createdAt)}
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <EventsShow events={events} navigate={navigate} donor={false}/>
   );
 };
 
